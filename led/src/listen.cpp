21cd61bbb500472acbca8d5f596ba7b30d7ce219
IDIOTSAD <55529455+IDIOTSAD@users.noreply.github.com>
2021-01-18 16:23:33 +0900

    Update listen.cpp

--- led/src/listen.cpp
+++ led/src/listen.cpp
@@ -1,22 +1,24 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
-void buttonCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("LED is : [%s]", msg->data.c_str());
-  std_msgs::String msg;
-  msg.data = "LEDON";
-  led_pub.publish(msg);
-}
+void buttonCallback(const std_msgs::String::ConstPtr& msg);
+
+ros::NodeHandle nh;
+ros::Publisher led_pub = nh.advertise<std_msgs::String>("led", 1000);
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener");
-  ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("button", 1000, buttonCallback);
-  ros::Publisher led_pub = nh.advertise<std_msgs::String>("led", 1000);
-  
   
   ros::spin();
   return 0;
+}
+
+void buttonCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("LED is : [%s]", msg->data.c_str());
+  std_msgs::String msg_;
+  msg_.data = "LEDON";
+  led_pub.publish(msg_);
 }
