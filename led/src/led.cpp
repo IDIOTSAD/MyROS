52aa9d5bcb45df0f48638026017c16bd5a18e066
IDIOTSAD <55529455+IDIOTSAD@users.noreply.github.com>
2021-01-18 15:58:12 +0900

    Update led.cpp

--- led/src/led.cpp
+++ led/src/led.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "led");
     ros::NodeHandle nh;
     ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("Message", 1000);
-
+    
     std_msgs::String msg;
 
     wiringPiSetupGpio();
@@ -23,16 +23,20 @@ int main(int argc, char **argv)
         if (digitalRead(SW_PIN))
         {
             msg.data = "ON";
+            /*
             digitalWrite(LED_PIN, HIGH);
             ROS_INFO("Set GPIO HIGH");
+            */
             ros::Duration(1.0).sleep();
             chatter_pub.publish(msg);
         }
         else
         {
             msg.data = "OFF";
+            /*
             digitalWrite(LED_PIN, LOW);
             ROS_INFO("Set GPIO LOW");
+            */
             ros::Duration(1.0).sleep();
             chatter_pub.publish(msg);
         }
